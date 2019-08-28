8f968bcdee05d2df62827c8deaa343f61dcb6f6e
Kevin <37988868+kdfriedman@users.noreply.github.com>
2019-08-28 19:21:36 -0400

    Add files via upload

---
+++ mailchimp-api-3/mailchimp-api-exec.php
@@ -0,0 +1,26 @@
+<?php
+    /* Template Name: mailchimp-api-exec */
+
+    // Exit if accessed directly
+    if ( ! defined( 'ABSPATH' ) ) {
+        exit;
+    }
+
+get_header('mc'); ?>
+
+<?php remove_filter ('the_content', 'wpautop'); ?>
+
+<?php
+// YOUR POST LOOP STARTS HERE
+while ( have_posts() ) : the_post(); ?>
+
+<?php
+// Display the post content
+// Note the "entry" class this is used for styling purposes so it's important to use it on any content element ?>
+
+    <?php the_content(); ?>
+
+
+<?php
+// YOUR POST LOOP ENDS HERE
+endwhile; ?>
